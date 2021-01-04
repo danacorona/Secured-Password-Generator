7bc55fa1cebfd1e59f9b533bc1ba4fd110ebf3fd
Dana Corona <dana.d.corona@gmail.com>
2021-01-04 16:48:41 -0600

    Updated the index.html file with a few comments to make sure the code is easy to understand. For the script.js file, I added a few alerts and confirms and tested them on the console to ensure functioning correctly.

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+alert ("Welcome to the Password Generator!");
+alert ("Let's start off with a few questions!");
+var uppercase = confirm ("Do you want to add uppercase characters?");
+var lowercase = confirm ("Do you want to add lowercase characters?");
+var numb = confirm ("Do you want to add numbers?");
+var spec = confirm ("Do you want to add special characters?");
+
+// Making sure that the responses are correct
+console.log(uppercase);
+console.log(lowercase);
+console.log(numb);
+console.log(spec);
