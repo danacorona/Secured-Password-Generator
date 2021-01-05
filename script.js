d413188c49b4654b70bc170e61f60f9604297157
Dana Corona <dana.d.corona@gmail.com>
2021-01-04 23:53:36 -0600

    Added FOR loops for randomizing the characters and tested them in the console. Updated the style.css with a note seperating the media queries.

--- script.js
+++ script.js
@@ -13,35 +13,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
 //Ask user data needed for generating password
 
-// alert ("Welcome to the Password Generator!");
-// alert ("Let's start off with a few questions!");
-// var uppercase = confirm ("Do you want to add uppercase characters?");
-// var lowercase = confirm ("Do you want to add lowercase characters?");
-// var numb = confirm ("Do you want to add numbers?");
-// var spec = confirm ("Do you want to add special characters?");
+alert ("Welcome to the Password Generator!");
+alert ("Let's start off with a few questions!");
+var uppercase = confirm ("Do you want to add uppercase characters? If so, press Okay");
+var lowercase = confirm ("Do you want to add lowercase characters? If so, press Okay");
+var numb = confirm ("Do you want to add numbers? If so, press Okay");
+var spec = confirm ("Do you want to add special characters? If so, press Okay");
 var length = prompt ("How long do you need your password to be?");
 
 // Making sure that the responses are correctly storing
-// console.log(uppercase);
-// console.log(lowercase);
-// console.log(numb);
-// console.log(spec);
-// console.log(length);
+console.log(uppercase);
+console.log(lowercase);
+console.log(numb);
+console.log(spec);
+
+// Checking to see if the user's input is a string or a number
+var length = parseInt(length);
+console.log(length);
+console.log(typeof(length));
 
 // Character Variables
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specChar = "!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~";
 
-//Checking Variables show up
-// console.log(lowerChar);
-// console.log(upperChar);
-// console.log(numbChar);
-// console.log(specChar);
-
-// console.log(Math.floor(Math.random() * lowerChar.length) +1);
 
 // for looping length times random numbers
   for (var i = 0; i < length; i++) {
@@ -49,13 +47,30 @@ var specChar = "!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~";
     console.log(numbChar);
    }
 
+  //  Testing the functions, may potentially use this and combine it with the other variables to generate a password
+  //  function randomUpper () {
+  //    return lowerChar[Math.floor(Math.random() *lowerChar.length)]
+  //  }
+  //  console.log("------");
+  //  console.log(randomUpper());
+  //  console.log("------");
+
    // for looping length times random lower case letters
    for (var i = 0; i < length; i++) {
      var newLowerChar = lowerChar[Math.floor(Math.random() * lowerChar.length)];
     console.log(newLowerChar);
    }
 
+   // for looping length times random lower case letters
    for (var i = 0; i < length; i++) {
     var newUpperChar = upperChar[Math.floor(Math.random() * upperChar.length)];
    console.log(newUpperChar);
    }
+
+   // symbol randomizer
+   function randomSymbol () {
+     const symbols = "!@#$%^&*()-_=+.><,/?';:";
+     return symbols[Math.floor(Math.random() * symbols.length)]
+   }
+
+   console.log(randomSymbol());
